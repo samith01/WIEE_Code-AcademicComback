3cef302a3efc8a1f227c74caf23790dc35d6042e
samith <samithfernando539@gmail.com>
2025-11-22 15:19:54 -0500

    update navbar with new name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h1 className="logo">WIEEE CODE</h1>
+      <h1 className="logo">AcademicComback</h1>
       <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/about">Project Page</Link>
